349379c4214d7aa312fd8e650c55c449735828f9
pulinapasan39 <pulinapasan39@gmail.com>
2026-04-24 05:33:40 +0530

    Create LinkedResourceNotFoundException.java

---
+++ src/main/java/com/smartcampus/api/LinkedResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.smartcampus.api;
+
+public class LinkedResourceNotFoundException extends RuntimeException {
+    
+    public LinkedResourceNotFoundException(String message) {
+        super(message);
+    }
+}
